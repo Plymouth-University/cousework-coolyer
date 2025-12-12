b0cbeba1e721aae1c0afc10d0ec7820034d61615
Josh <35643522+coolyer@users.noreply.github.com>
2025-12-12 04:16:18 +0000

    Delete backend directory

--- backend/models/Booking.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bookingSchema = new mongoose.Schema({
-  room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
-  guestName: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Booking', bookingSchema);
