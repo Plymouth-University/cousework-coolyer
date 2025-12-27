6da0380e439b51b91826436926bc92d2a73c741f
Josh <35643522+coolyer@users.noreply.github.com>
2025-12-27 18:51:30 +0000

    Delete removeme.js

--- removeme.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/components/__tests__/RoomList.test.js
-import { render, screen } from '@testing-library/react';
-import RoomList from '../RoomList';
-
-test('renders room list', () => {
-  render(<RoomList rooms={[{ number: 101, type: 'Single', price: 100 }]} />);
-  expect(screen.getByText(/Single/)).toBeInTheDocument();
-});
