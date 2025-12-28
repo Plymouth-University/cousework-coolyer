762c85cf8644265019b1bfee9e366690d53f4bd6
Josh Laptop <josh.warburton@yahoo.com>
2025-12-28 04:50:29 +0000

    Added drop down to edit rooms

--- admin_frontend/src/components/EditRoom.js
+++ admin_frontend/src/components/EditRoom.js
@@ -25,7 +25,16 @@ function EditRoom({ room, onSave, onCancel }) {
   return (
     <form onSubmit={handleSubmit} style={{ margin: '10px 0' }}>
       <input value={number} onChange={e => setNumber(e.target.value)} placeholder="Room Number" />
-      <input value={type} onChange={e => setType(e.target.value)} placeholder="Type" />
+      <select
+        value={type}
+        onChange={e => setType(e.target.value)}
+        style={{ marginRight: '10px' }}
+      >
+        <option value="">Select Type</option>
+        <option value="Single">Single</option>
+        <option value="Double">Double</option>
+        <option value="Twin">Twin</option>
+      </select>
       <input value={price} onChange={e => setPrice(e.target.value)} placeholder="Price" type="number" />
       <button type="submit">Save</button>
       <button type="button" onClick={onCancel} style={{ marginLeft: 8 }}>Cancel</button>
